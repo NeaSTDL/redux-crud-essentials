2ca621d0a7a235099fac8db17e26433422fa7226
Emir Salazar <emir.salazar@nimbusnine.co>
2019-06-27 15:59:39 -0500

    RDXC: Adding Babel, XO and Webpack initial settings. RDXC: Creating initial structure for the project.

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+// Preset defined to:
+//  - Allow Jest to use Babel for testing
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current'
+        }
+      }
+    ]
+  ]
+};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+export function createReduxCrud() {
+  return {};
+}
+
+export default createReduxCrud;
